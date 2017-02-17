fe62c829fb8f49074fda531809172f8ee70f838d
Hassam Ali <hassam.ali7@hotmail.com>
2017-02-17 18:03:16 +0500

    FormRequest Validation class created

---
+++ app/Http/Requests/SignUpRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SignUpRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'inputName'=>'required|max:10|min:2|string',
+            'userEmail'=>'required|max:20|min:2|email|unique:users,email',
+            'userPassword'=>'required|min:6|confirmed',
+            'country'=>'required'
+        ];
+    }
+}
