331909f44864e057032cceb89a1a47534f8e11ae
Hassam Ali <hassam.ali7@hotmail.com>
2017-02-18 00:20:23 +0500

    Seeder for User and Quotes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +11,25 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $faker = Faker\Factory::create();
+        $limit = 10;
+        for ($i = 0; $i < $limit; $i++) {
+            $res =  DB::table('users')->insertGetId([ //,
+                'name' => $faker->name,
+                'email' => $faker->unique()->email,
+                'password' => $faker->userName,
+                'country'=>$faker->country
+            ]);
+
+            for($inner =0;$inner<4;$inner++){
+                DB::table('quotes')->insert([
+                    'user_id'=>$res,
+                    'text'=>$faker->text($maxNbChars = 200),
+                    'author'=>$faker->lastName
+                ]);
+            }
+            $this->command->info($res);
+
+        }
     }
 }
