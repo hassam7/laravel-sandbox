8181372278c3dd46d65d05237444b813a8b06572
Hassam Ali <hassam.ali7@hotmail.com>
2017-02-17 22:47:08 +0500

    Login Form.
    This is not a good way to implment login form, laravel already comes with authentication framework which is much better than implementing hand coded login form. Even middleware would be better way to go

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use	Illuminate\Support\facades\Input;
+use Illuminate\Support\Facades\Validator;
 
+use App\User;
 class LoginController extends Controller
 {
     //
+    public function index(){
+        return view('login.login');
+    }
+
+    public function login(){
+        $rules = [
+                'userEmail'=>'required|email',
+                'userPassword'=>'required',
+            ];
+
+        $validator = Validator::make(Input::all(),$rules);
+        $email = Input::get('userEmail');
+        $password = Input::get('userPassword');
+        $temp = User::where('email',$email)->where('password',$password)->get();
+        if(!$temp->isEmpty()){
+            return "Logged In";
+        }else{
+          return view('login.login')->withErrors("Invalid Username or Password<br/>Try Again");
+        }
+    }
 }
