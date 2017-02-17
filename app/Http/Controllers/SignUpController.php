b2f4be3791a3158e06c30f5a331f2102f115bdd8
Hassam Ali <hassam.ali7@hotmail.com>
2017-02-17 18:03:37 +0500

    Replace manual validation with FormRequest Validation

--- app/Http/Controllers/SignUpController.php
+++ app/Http/Controllers/SignUpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Http\Requests\SignUpRequest;
 class SignUpController extends Controller
 {
     //
@@ -11,13 +12,8 @@ class SignUpController extends Controller
         return view('signup.signup');
     }
 
-    public function create(Request $request){
-        $temp = $this->validate($request,[
-            'inputName'=>'required|max:10|min:2|string',
-            'userEmail'=>'required|max:20|min:2|email|unique:users,email',
-            'userPassword'=>'required|min:6|confirmed',
-            'country'=>'required'
-        ]);
+    public function create(SignUpRequest $request){
+        
         $u = new User();
         $u->name = $request->inputName;
         $u->email = $request->userEmail;
@@ -26,4 +22,26 @@ class SignUpController extends Controller
         $u->save();    
         return view('signup.signup')->with('message','User created successfully');
     }
+
+
+    /*
+        Validation using auto redirect with manually writing to model
+        
+        public function create(Request $request){
+            $temp = $this->validate($request,[
+                'inputName'=>'required|max:10|min:2|string',
+                'userEmail'=>'required|max:20|min:2|email|unique:users,email',
+                'userPassword'=>'required|min:6|confirmed',
+                'country'=>'required'
+            ]);
+            $u = new User();
+            $u->name = $request->inputName;
+            $u->email = $request->userEmail;
+            $u->password = $request->userPassword;
+            $u->country = $request->country;
+            $u->save();    
+            return view('signup.signup')->with('message','User created successfully');
+        }
+
+    */
 }
