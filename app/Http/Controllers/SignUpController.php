3bc76c94a765f3add601ab458b5b07fb405d1fb1
Hassam Ali <hassam.ali7@hotmail.com>
2017-02-17 19:03:06 +0500

    Manual Validation with Validator

--- app/Http/Controllers/SignUpController.php
+++ app/Http/Controllers/SignUpController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Http\Requests\SignUpRequest;
+
+use Illuminate\Support\facades\Input;
+use Illuminate\Support\Facades\Validator;
+
 class SignUpController extends Controller
 {
     //
@@ -12,8 +16,36 @@ class SignUpController extends Controller
         return view('signup.signup');
     }
 
+    public function create(){
+          
+            $rules = array(
+                'inputName'=>'required|max:10|min:2|string',
+                'userEmail'=>'required|max:20|min:2|email|unique:users,email',
+                'userPassword'=>'required|min:6|confirmed',
+                'country'=>'required'
+            );
+            $validator = Validator::make(Input::all(),$rules);
+
+            if($validator->fails()){
+                $messages = $validator->messages();
+                return redirect()->back()->withErrors($validator)->withInput();
+            }
+            else{
+                $u = new User();
+                $u->name = Input::get('inputName'); 
+                $u->email = Input::get('userEmail');
+                $u->password = Input::get('userPassword');
+                $u->country = Input::get('country');
+                $u->save();    
+                return view('signup.signup')->with('message','User created successfully');
+                
+            }
+    }
+
+/*
+Validation Using FormRequest
     public function create(SignUpRequest $request){
-        
+            
         $u = new User();
         $u->name = $request->inputName;
         $u->email = $request->userEmail;
@@ -22,8 +54,7 @@ class SignUpController extends Controller
         $u->save();    
         return view('signup.signup')->with('message','User created successfully');
     }
-
-
+*/
     /*
         Validation using auto redirect with manually writing to model
         
