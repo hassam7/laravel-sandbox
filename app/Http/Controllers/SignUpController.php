71ffce997405c969bafb8e3a40609bf522ae9b44
Hassam Ali <hassam.ali7@hotmail.com>
2017-02-17 17:44:47 +0500

    SignUpController saves data to database via Eloquent Models

--- app/Http/Controllers/SignUpController.php
+++ app/Http/Controllers/SignUpController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\User;
 class SignUpController extends Controller
 {
     //
@@ -18,7 +18,12 @@ class SignUpController extends Controller
             'userPassword'=>'required|min:6|confirmed',
             'country'=>'required'
         ]);
-       
+        $u = new User();
+        $u->name = $request->inputName;
+        $u->email = $request->userEmail;
+        $u->password = $request->userPassword;
+        $u->country = $request->country;
+        $u->save();    
         return view('signup.signup')->with('message','User created successfully');
     }
 }
