84ab69d8d7eb975d31900eacb84e9afd2e68c903
Hassam Ali <hassam.ali7@hotmail.com>
2017-02-17 16:54:26 +0500

    Add Country Field and Confirm Password

--- resources/views/signup/signup.blade.php
+++ resources/views/signup/signup.blade.php
@@ -25,21 +25,29 @@
        @endif
         
 
-       <div class="alert alert-success alert-dismissable">
-            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-            <strong>Success!</strong> Indicates a successful or positive action.
-        </div>
+       
         <form class="form-signup" method="post" action="/signup">
             <h2 class="form-signup-heading">Sign Up Form</h2>
+             <div class="form-group">
                 <label for="inputName" class="sr-only">Your Full Name</label>
                 <input type="text" id="inputName" name="inputName" class="form-control" placeholder="Full Name" required autofocus>
-
+            </div>
+             <div class="form-group">
                 <label for="inputEmail" class="sr-only">Email address</label>
                 <input type="email" id="inputEmail" name="userEmail" class="form-control" placeholder="Email address" required autofocus>
-
+</div>
+ <div class="form-group">
                 <label for="inputPassword" class="sr-only">Password</label>
                 <input type="password" name="userPassword" id="inputPassword" class="form-control" placeholder="Password" required>
-
+</div>
+ <div class="form-group">
+                <label for="inputConfPassword" class="sr-only">Password</label>
+                <input type="password" name="inputConfPassword" id="inputConfPassword" class="form-control" placeholder="Confirm Password" required>
+</div>
+ <div class="form-group">
+                <label for="country" class="sr-only">Country</label>
+                <input type="text" name="country" id="country" class="form-control" placeholder="Country" required>
+</div>
                 <button class="btn btn-primary btn-block" type="submit" id="formbtn">Next</button>
                 {{ csrf_field() }}
         </form>
