30c0a0ddddf65f9661b39ee1f2ece26f77193b17
Hassam Ali <hassam.ali7@hotmail.com>
2017-02-18 01:15:43 +0500

    Intial views for QuotesController

---
+++ resources/views/authorized/master.blade.php
@@ -0,0 +1,5 @@
+Master Page:It will display list of quotes with
+    detail button:  To view detail of particular item
+    update button:  To Update selected  item
+    add    button:  To add new button
+    delete button:  To delete quote
