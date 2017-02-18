c9d567a6b41c86f152135078bffcf2637a700937
Hassam Ali <hassam.ali7@hotmail.com>
2017-02-18 18:24:38 +0500

    create blade template for update action

---
+++ resources/views/authorized/update.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Update Quote</title>
+
+    <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+    <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
+</head>
+<style>
+    .container {
+        padding-top: 90px;
+    }
+</style>
+
+<body>
+    <div class="container">
+        <div class="col-md-3">
+            <ul class="list-unstyled">
+                <li><a href="{{route('home')}}">Home</li>            
+                <li><a href="{{route('create')}}">Add New Quote</li>
+                <li><a href="{{route('logoff')}}">Log Out</a></li </ul>
+        </div>
+        <div class="col-md-9">
+                
+        
+        </div>
+    </div 
+</body>
+
+</html>
