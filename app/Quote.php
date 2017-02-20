4432ba08953eeabf9af180cb8a778d3f64e81f6e
Hassam Ali <hassam.ali7@hotmail.com>
2017-02-20 05:59:13 +0500

    Eloquent Accessor with backing field

--- app/Quote.php
+++ app/Quote.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Quote extends Model
 {
     //
+    function scopeGetCurrentUsers($query,$currentUserId){
+        return $query->where('user_id',$currentUserId);
+    }
+
+    function getAuthorAttribute($value){
+        return strtoupper($value);
+    }
 }
