a70ef11e5124ae8a0336cb302fedb2906d4186bb
Hassam Ali <hassam.ali7@hotmail.com>
2017-02-20 09:38:20 +0500

    eloquent accessor

--- app/Quote.php
+++ app/Quote.php
@@ -10,8 +10,9 @@ class Quote extends Model
     function scopeGetCurrentUsers($query,$currentUserId){
         return $query->where('user_id',$currentUserId);
     }
-
+    //example of eloquent accessor with backing field
     function getAuthorAttribute($value){
         return strtoupper($value);
     }
+
 }
